0bdbc23ca8034a9918df097fddd5408105079e25
Monty Anderson <montydev@protonmail.com>
2017-10-19 13:56:31 +0100

    Hello, World!

---
+++ main.rs
@@ -0,0 +1,16 @@
+use std::io::Write;
+use std::net::TcpListener;
+use std::thread;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:9123").unwrap();
+    println!("Listener started!");
+
+    for stream in listener.incoming() {
+        thread::spawn(|| {
+            let mut stream = stream.unwrap();
+
+            stream.write(b"Hello, World!").unwrap();
+        });
+    }
+}
